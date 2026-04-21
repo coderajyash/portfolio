28a1ad7c1f11eec407257972bc24839d268ab804
yashraj <yashrajsingh.dg@gmail.com>
2026-04-22 00:14:55 +0100

    Hero Content Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants';
-import profilePic from '../assets/profilePic.jpeg';
+import profilePic from '../assets/profilePic.png';
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
